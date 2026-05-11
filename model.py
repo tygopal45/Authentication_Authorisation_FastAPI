f4c3292ecc0e6c845c939c9a7d1ecc790abb1627
tygopal45 <jeegopal835@gmail.com>
2026-05-11 13:36:21 +0530

    created table schema

---
+++ model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, VARCHAR
+from database import Base
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(VARCHAR(255))
+    author = Column(VARCHAR(255))
+    publish_date = Column(VARCHAR(255))
+    
+
