f5649b83cfce05548d6ac94f8d41a5cf186f7c2d
tygopal45 <jeegopal835@gmail.com>
2026-05-19 11:18:26 +0530

    added user login schema

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel, EmailStr
 
+# schema for new user registration
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
     username: str
     role: str
+
+# schema for user login
+class UserLogin(BaseModel):
+    username: str
+    password: str
 
 
 
